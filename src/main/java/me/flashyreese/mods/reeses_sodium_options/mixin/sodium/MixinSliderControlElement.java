f4bcedd97bdfa03c2a4e5747a783a30ce68aeb79
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2024-07-23 03:26:55 -0500

    change: Allow using the space key to activate elements

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/mixin/sodium/MixinSliderControlElement.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/mixin/sodium/MixinSliderControlElement.java
@@ -3,21 +3,18 @@ package me.flashyreese.mods.reeses_sodium_options.mixin.sodium;
 import me.flashyreese.mods.reeses_sodium_options.client.gui.SliderControlElementExtended;
 import me.jellysquid.mods.sodium.client.gui.options.Option;
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlElement;
-import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
 import me.jellysquid.mods.sodium.client.util.Dim2i;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.CommonInputs;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.util.Mth;
 import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(targets = "me.jellysquid.mods.sodium.client.gui.options.control.SliderControl$Button")
 public abstract class MixinSliderControlElement extends ControlElement<Integer> implements SliderControlElementExtended {
@@ -34,14 +31,7 @@ public abstract class MixinSliderControlElement extends ControlElement<Integer> 
     private int min;
 
     @Unique
-    private int max;
-
-    @Unique
     private boolean editMode;
-
-    @Shadow
-    @Final
-    private int range;
 
     public MixinSliderControlElement(Option<Integer> option, Dim2i dim) {
         super(option, dim);
@@ -60,61 +50,33 @@ public abstract class MixinSliderControlElement extends ControlElement<Integer> 
     @Shadow
     public abstract double getThumbPositionForValue(int value);
 
+    @Mutable
     @Shadow
-    public abstract int getIntValue();
+    @Final
+    private Rect2i sliderBounds;
 
-    @Shadow public abstract void setValue(double d);
+    @Shadow
+    @Final
+    private int max;
 
-    @Inject(method = "<init>", at = @At(value = "TAIL"))
-    public void postInit(Option<Integer> option, Dim2i dim, int min, int max, int interval, ControlValueFormatter formatter, CallbackInfo ci) {
-        this.max = max;
+    @Inject(method = "render", at = @At(value = "HEAD"))
+    public void render(GuiGraphics drawContext, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        this.sliderBounds = new Rect2i(this.dim.getLimitX() - 96, this.dim.getCenterY() - 5, 90, 10);
     }
 
-    @Unique
-    private Dim2i getSliderBounds() { // fixme: insanity
-        return new Dim2i(this.dim.getLimitX() - 96, this.dim.getCenterY() - 5, 90, 10);
-    }
-
-    @Inject(method = "renderSlider", at = @At(value = "TAIL"))
-    public void rso$renderSlider(GuiGraphics guiGraphics, CallbackInfo ci) {
-        int sliderX = this.getSliderBounds().x();
-        int sliderY = this.getSliderBounds().y();
-        int sliderWidth = this.getSliderBounds().width();
-        int sliderHeight = this.getSliderBounds().height();
-        this.thumbPosition = this.getThumbPositionForValue(this.option.getValue());
-        double thumbOffset = Mth.clamp((double) (this.getIntValue() - this.min) / (double) this.range * (double) sliderWidth, 0.0, sliderWidth);
-        double thumbX = (double) sliderX + thumbOffset - 2.0;
+    @Inject(method = "renderSlider", at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    public void rso$renderSlider(GuiGraphics guiGraphics, CallbackInfo ci, int sliderX, int sliderY, int sliderWidth, int sliderHeight, double thumbOffset, int thumbX, int trackY, String label, int labelWidth) {
         if (this.isFocused() && this.isEditMode()) {
-            this.drawRect(guiGraphics, (int) (thumbX - 1), sliderY - 1, (int) (thumbX + 5), sliderY + sliderHeight + 1, 0xFFFFFFFF);
+            this.drawRect(guiGraphics, thumbX - 1, sliderY - 1, thumbX + 5, sliderY + sliderHeight + 1, 0xFFFFFFFF);
         }
-    }
-
-    @Redirect(method = "renderStandaloneValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Rect2i;getX()I"))
-    public int rso$renderStandaloneValueSliderBoundsGetX(Rect2i instance) {
-        return this.getSliderBounds().x();
-    }
-
-    @Redirect(method = "renderStandaloneValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Rect2i;getY()I"))
-    public int renderStandaloneValueSliderBoundsGetY(Rect2i instance) {
-        return this.getSliderBounds().y();
-    }
-
-    @Redirect(method = "renderSlider", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Rect2i;getX()I"))
-    public int rso$renderSliderSliderBoundsGetX(Rect2i instance) {
-        return this.getSliderBounds().x();
-    }
-
-    @Redirect(method = "renderSlider", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Rect2i;getY()I"))
-    public int rso$renderSliderSliderBoundsGetY(Rect2i instance) {
-        return this.getSliderBounds().y();
     }
 
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
         if (!isFocused()) return false;
 
-        if (keyCode == GLFW.GLFW_KEY_ENTER) {
-            this.setEditMode(!this.isEditMode());;
+        if (CommonInputs.selected(keyCode)) {
+            this.setEditMode(!this.isEditMode());
             return true;
         }
 
@@ -131,17 +93,6 @@ public abstract class MixinSliderControlElement extends ControlElement<Integer> 
         return false;
     }
 
-    @Inject(method = "setValueFromMouse", at = @At(value = "HEAD"), cancellable = true, remap = false) // fixme: insanity part 3
-    public void rso$setValueFromMouse(double d, CallbackInfo ci) {
-        this.setValue((d - (double)this.getSliderBounds().x()) / (double)this.getSliderBounds().width());
-        ci.cancel();
-    }
-
-    @Redirect(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Rect2i;contains(II)Z"))
-    public boolean rso$mouseClicked(Rect2i instance, int x, int y) {
-        return this.getSliderBounds().containsCursor(x, y);
-    }
-
     @Unique
     private void setValueFromMouseScroll(double amount) {
         if (this.option.getValue() + this.interval * (int) amount <= this.max && this.option.getValue() + this.interval * (int) amount >= this.min) {
@@ -152,7 +103,7 @@ public abstract class MixinSliderControlElement extends ControlElement<Integer> 
 
     @Override
     public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
-        if (this.option.isAvailable() && this.getSliderBounds().containsCursor(mouseX, mouseY) && Screen.hasShiftDown()) {
+        if (this.option.isAvailable() && this.sliderBounds.contains((int) mouseX, (int) mouseY) && Screen.hasShiftDown()) {
             this.setValueFromMouseScroll(verticalAmount); // todo: horizontal separation
 
             return true;
